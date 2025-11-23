04dad6b86c1debdad6cdd1a49c9a1d1feb2a7ddf
Aaron Williams <osmosis2k15@gmail.com>
2025-11-23 14:03:58 -0700

    Trying a new route

--- DigitalDash/MainWindow.xaml.cs
+++ DigitalDash/MainWindow.xaml.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.Windows;
+using DigitalDash.ViewModels;
+
+namespace DigitalDash
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();      // generated from XAML
+            DataContext = new MainViewModel();
+        }
+    }
+}
